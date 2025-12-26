347eb261b4a7ed6d26aec3eababd9e05d4287b81
Jie <evgeniagaigerova@yandex.ru>
2025-12-26 19:26:30 +0300

    initiation

---
+++ app.py
@@ -0,0 +1,14 @@
+# Импортируем Flask
+from flask import Flask, render_template  
+
+# Создаём экземпляр приложения Flask
+app = Flask(__name__)  
+
+# Определяем маршрут для главной страницы
+@app.route("/")
+def home():
+    return render_template("index.html")  
+
+# Запускаем сервер
+if __name__ == "__main__":
+    app.run(debug=True)  # debug=True включает автоматическую перезагрузку
