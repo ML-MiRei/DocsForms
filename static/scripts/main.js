d4a2d952f88bab3e883f218872c6cac0698aab14
Jie <evgeniagaigerova@yandex.ru>
2026-01-02 17:18:53 +0300

    main page

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,3 +4,30 @@ var menu = document.querySelector('.menu__list');
 openMenuButton.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+    },
+});
